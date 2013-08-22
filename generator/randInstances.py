2fcc2367304e04d416eba1a0ef9456a7a312ebea
Marcos Daniel Valadao Baroni <mbaroni@nyx.ninfa.inf.ufes.br>
2013-08-22 19:49:08 -0300

    Distribuicao quase calibrada.

--- generator/randInstances.py
+++ generator/randInstances.py
@@ -3,6 +3,36 @@ import random as r
 import math
 import sys
 
+def sign(x):
+	if x > 0. : return  1
+	if x < 0. : return -1
+	return 0
+
+def over_line(g):
+	(ax, ay) = (math.log(150.0), math.log(120000.))
+	(bx, by) = (math.log(20000.), math.log(200.))
+	b = (bx*ay - ax*by)/(bx - ax)
+	a = (by-b)/bx
+	if g == 1:
+		x = (0.35 + 0.1*r.gauss(0.5, 0.3))*(-b/a)
+	elif g == 2:
+		x = (0.6 + 0.1*r.gauss(0.5, 0.3))*(-b/a)
+	y = a*x + b
+	y *= r.gauss(1., 0.1)
+	return (x, y)
+
+def exp_gauss():
+	x = -1
+	med = 100
+	d = 5
+	while x < 0. or x > 1.:
+		aux = med*0.1 + r.gauss(0, 0.5)*med
+		x = 0.1 + (sign(aux)*math.exp(abs(aux-0.1*med)))/med
+	return x
+
+def rand_cost_uc(rotate=False):
+	return (r.random(), exp_gauss())
+
 class Action:
 	years = 0
 	cost = 0.0
@@ -11,15 +41,12 @@ class Action:
 	curve = []
 	def __init__(self, inst):
 		self.inst = inst
-		self.years = years
+		self.years = self.inst.years
 		self.tir = 15 + math.exp(r.gauss(0,1.2))*45
 		#self.tir = 15 + r.random()*45 + math.exp(r.gauss(0,1.2))*45
-		if r.randint(1,2) == 1:
-			# Alto Custo
-			self.uc = r.random()
-		else:
-			# Alto UC
 		self.set_curve(r.randint(1,3))
+		#self.cost, self.uc = over_line(r.randint(1,2))
+		self.cost, self.uc = rand_cost_uc(r.randint(0,1) == 1)
 	
 	def set_curve(self, c):
 		years = self.inst.years
@@ -30,6 +57,17 @@ class Action:
 		elif c == 3:
 			self.curve = [0.5, 0.25, 015] + [0.0]*(years-3)
 	
+	def to_gplot(self):
+		s = str(self.cost) + " "
+		s += str(self.uc) + " "
+		s += str(self.tir)
+		return s
+	
+	def set_group(self, g):
+		self.group = g
+		if g == 1: self.cost, self.uc = over_line(1)
+		elif g == 2 : self.cost, self.uc = over_line(2)
+
 	def __str__(self):
 		s = "Curve:\n" + str(self.curve)
 		return s
@@ -41,14 +79,17 @@ class Instance:
 	budgets = []
 	goals = []
 	acts = []
+	stddist = [.36, .36, .14, .14] # Distribution for respective action group
+	dist = [] # number of actions from respective group
 	def __init__(self, seed, alpha, years, camp):
 		r.seed(seed)
 		self.years = years
 		self.camp = camp
 		self.alpha = alpha
-		self.set_budgets()
-		self.set_goals()
-		self.set_acts()
+		self.set_budgets()		# yearly budgets
+		self.set_goals()		# yearly goals
+		for i in range(self.camp):
+			self.acts.append(Action(self))
 	
 	def set_budgets(self):
 		medianOrc = r.randint(700,750)
@@ -60,9 +101,30 @@ class Instance:
 		for i in range(self.years):
 			self.goals.append(0.0)
 
-	def set_acts(self):
-		for i in range(self.camp):
-			self.acts.append(Action(self.years, self.alpha, self))
+	def set_dist(self):
+		na = self.camp-4
+		int(round(gauss(na,1)))
+	
+	def gplot(self):
+		s = ""
+		#s += "set term dumb\n"
+		#s += "set logscale x\n"
+		#s += "set logscale y\n"
+		#s += "set xrange [0:100]\n"
+		s += "set yrange [0:1]\n"
+		#s += "set xrange [100:30000]\n"
+		#s += "set yrange [100:1000000]\n"
+		s += "set xlabel \"Invest\"\n"
+		s += "set ylabel \"UCs\"\n"
+		s += "set zlabel \"TIR\"\n"
+		s += "set grid xtics ytics ztics\n"
+		s += "plot '-' using 1:2 with points pointsize 3\n"
+		for a in self.acts:
+			s += a.to_gplot() + "\n"
+		s += "e e e\n"
+		s += "pause -1\n"
+		return s
+
 
 	def to_scip(self):
 		print self.years
@@ -76,6 +138,6 @@ class Instance:
 		for a in self.acts:
 			print a
 
-inst = Instance(1, 0.0, 4, 5)
-inst.to_scip()
+inst = Instance(None, 0.0, 4, 100)
+print inst.gplot()
 
--- real/real.plot
+++ real/real.plot
@@ -1,12 +1,13 @@
 #set logscale y;plot "< awk '{print $3}' real.csv|sort" title "TIR"
-#set logscale x
-#set logscale y
+set logscale x
+set logscale y
 #set logscale z
+set xrange [100:30000]
 set xlabel "Invest"
 set ylabel "UCs"
-set zlabel "TIR"
+#set zlabel "TIR"
 set grid xtics ytics ztics
-splot 'real.csv' using 1:2:3 with points pointsize 3
+plot 'real.csv' using 1:2 with points pointsize 3
 
 pause -1
 
