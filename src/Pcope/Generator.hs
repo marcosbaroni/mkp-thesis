c9d386858460ce0ced3569f206fd875e8eee4ddf
Marcos Daniel Baroni <mbaroni@mbaroni.(none)>
2014-01-08 23:57:44 -0200

    Iniciando geração aleatória.

--- src/Pcope/Generator.hs
+++ src/Pcope/Generator.hs
@@ -1,15 +1,35 @@
 {-# LANGUAGE Rank2Types #-}
 module Pcope.Generator where
 
-import Pcope.Instance
 import System.Random (RandomGen)
+
+import Pcope.Instance
+import Pcope.Tools
 
 -- | Configurationg for the random generator.
 data Config = Config {
+	nActs :: Int,
 	nYears :: Int,
-	nActs :: Int
-	-- | ...
+	nPers :: Int,
+	nRes :: Int,
+	rate :: Double
 	}
 
 type Generator = (RandomGen a => a -> Config -> Pcope)
 
+{--
+stdGen :: Generator
+stdGen g c = pcope {
+		pN = nActs c,
+		pY = nYears c,
+		pP = nPers c,
+		pR = nRes c,
+		pr = rate c,
+		pg = pgRand,
+		po = poRand,
+		pp = ppRand,
+		ps = psRand,
+		pActs = pActsRand
+	}
+	where
+--}
