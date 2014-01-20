834bcd37e9353f30bd372843414de21a9f3c8335
Marcos Daniel Baroni <mbaroni@mbaroni-Aspire-1830T.(none)>
2014-01-20 02:34:52 -0300

    Alterada entrada de parametros da estrutura geradora.

--- src/c/generator.c
+++ src/c/generator.c
@@ -8,11 +8,11 @@ int main(int argc, char **argv){
 	PCOPE *p;
 	RandConf *rc;
 
-	srand(time(NULL));
 
-	rc = randconf_default();
-	p = pcope_random(5, 4, 3, 2, 0.15, rc);
+	rc = randconf_default(0, 5, 4, 3, 2, 0.15, 1.0);
+	p = pcope_random(rc);
 	//pcope_to_plain(p, stdout);
+	pcope_to_scip(p, stdout);
 
 	pcope_free(p);
 	randconf_free(rc);
