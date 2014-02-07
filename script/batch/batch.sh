aa717fc76154f43af700226d83e12276c260b5d2
Marcos Daniel Valadao Baroni <mbaroni@keris.(none)>
2014-02-07 11:38:36 -0200

    Ajustando script batch...

--- script/batch/batch.sh
+++ script/batch/batch.sh
@@ -3,13 +3,15 @@
 #
 
 # Parâmetros
-as=`seq 25 25 100`
-ys=`seq 3 1 11`
-ts=`seq 0.0 0.2 1.0`
-ss=`seq 1 1 100`
+as="25 50 75 100"
+ys="3 4 5 6 7 8 9 10 11"
+ts="0.0 0.2 0.4 0.6 0.8 1.0"
+ss=`seq -f %02.f 0 1 99`
 
 TMPDIR=`mktemp -d`        # Pasta temportária
 SOLDIR="solutions"        # Pasta para gravar as soluções
+INSTDIR=$1
+
 OUTCSV="result.csv"       # Arquivo csv com os resultados
 LOG="batch.log"           # Arquivo de log de execução
 
@@ -18,13 +20,13 @@ mkdir $SOLDIR
 
 echo "Salvando resultados em $OUTCSV, soluções em $SOLDIR/ e log em $LOG." | tee -a $LOG
 
-echo "\"acts\";\"years\";\"tir\";\"seed\";\"obj\";\"time\"" > $OUTCSV
+echo "\"acts\";\"years\";\"tir\";\"seed\";\"obj\";\"time\";\"gap\"" > $OUTCSV
 for a in $as do
 	for y in $ys do
 		for t in $ts do
 			for s in $ss do
 				# Definindo nome dos arquivos
-				input=$a/$y/$t/$s.zpl
+				input=$INSTDIR/$y/$a/$t/$s.zpl
 				outprefix=$a-$y-$t-$s
 
 				# Execução
@@ -32,6 +34,7 @@ for a in $as do
 
 				# Salvando as soluções
 				./scip2sol < $TMPDIR/scip.log > solutions/$outprefix.sol
+				./scip2obj < $TMPDIR/scip.log > solutions/$outprefix.obj
 				./scip2obj < $TMPDIR/scip.log > solutions/$outprefix.obj
 
 				# Escrevendo no CSV de resultados
