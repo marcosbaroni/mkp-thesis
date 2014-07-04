e6a5790ccecb78d8ed93182d9fccf631a04584e1
Marcos Daniel Valadao Baroni <mbaroni@nyx.ninfa.inf.ufes.br>
2014-07-04 14:15:48 -0300

    Ajustando modelo para multiknap.

--- src/mkp/c/util.h
+++ src/mkp/c/util.h
@@ -4,6 +4,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define NUMBER int
+
+int *read_int_array(FILE *in, int n);
+int **read_int_matrix(FILE *in, int n, int m);
+
+/* Printing */
+int *fprint_int_array(FILE *out, int *array, int n);
+int **fprint_int_matrix(FILE *out, int **mat, int n, int m);
+
+/* Freeing */
+void free_int_array(int *array);
+void free_int_matrix(int **mat, int n);
+
+/* random NUMBERs */
+double drand();
+double irand();
+
 /* ZIMPL FORMAT */
 void zimpl_print_matrix(FILE *fout, double **mat, int nlin, int ncol);
 void zimpl_print_array(FILE *fout, double *array, int n);
