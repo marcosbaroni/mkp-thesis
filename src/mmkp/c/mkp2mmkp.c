e173a7428af9240c348606144f5805daae6c8f5f
Marcos Daniel Baroni <mbaroni@mbaroni.(none)>
2014-07-15 12:27:24 -0300

    Implementing transformation from MKP to MMKP.

---
+++ src/mmkp/c/mkp2mmkp.c
@@ -0,0 +1,14 @@
+/***************************************************
+ * Transforms and MKP instance on a MMKP instance
+ *   by splitting knapsack capacities.
+ **************************************************/
+
+ #include <stdio.h>
+ #include <stlib.h>
+ #include "mmkp.h"
+
+int main(int argc, char **argv){
+	fprintf(stderr, "Not working yet.\n");
+	return 1;
+}
+
