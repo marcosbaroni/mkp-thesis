a5eb92b14113c434cf7ff0050a4885898eae42d9
Marcos Daniel Valadao Baroni <mbaroni@eos.ninfa.inf.ufes.br>
2014-08-28 16:30:01 -0300

    Lendo mkp2zpl de arquivo tbm.

--- src/mmkp/c/mkp2zpl.c
+++ src/mmkp/c/mkp2zpl.c
@@ -4,12 +4,20 @@
 #include "mmkp/mkp.h"
 #include "mmkp/util.h"
 
-int main(){
+int main(int argc, char **argv){
 	MKP *mkp;
-	mkp = mkp_read_from_file(stdin);
+	FILE *input;
+
+	input = stdin;
+	if( argc > 1) {
+		input = fopen(argv[1], "r");
+		printf("file %s\n", argv[1]);
+	}
+
+	mkp = mkp_read_from_file(input);
 	mkp_to_zimpl(stdout, mkp);
 	mkp_free(mkp);
 
-	return 1;
+	return 0;
 }
 
