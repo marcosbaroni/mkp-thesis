275db2299361bb7555fd6a5788c13974221dae08
Marcos Daniel Valadao Baroni <mbaroni@eos.ninfa.inf.ufes.br>
2014-11-11 14:17:17 -0200

    Batch script for kpbt experiments.

---
+++ experiments/kpbt/2014-11-11/exec-batch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+######################################################################
+# Script para testes do algoritmo de backtrack para o KP.
+######################################################################
+KPBT=../../../bin/kpbt
+
+# Checking number of arguments
+if [ $# -lt 2 ]; then
+	echo "usage.: $0 <inst directories> <output file>"
+	exit 1
+fi
+
+inputdir=$1
+outfile=$2
+
+echo "nitems;found;proof;profit;split;first;last;selected" > $outfile
+for n in `seq 10 30`
+do
+	echo $n
+	for i in `seq -f "%03.f" 1 1000`
+	do
+		$KPBT $inputdir/$n/$i.txt >> $outfile
+	done
+done
+
