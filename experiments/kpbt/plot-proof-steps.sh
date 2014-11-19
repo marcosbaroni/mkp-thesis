c20c3e50455a68129a1123292baaef6d66bb1865
Marcos Daniel Baroni <mbaroni@mbaroni.(none)>
2014-11-19 05:44:51 -0200

    feito graficos experimentais.

--- experiments/kpbt/plot-proof-steps.sh
+++ experiments/kpbt/plot-proof-steps.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-data=$1
+data1=$1
+data2=$2
 term=svg
 
 gnuplot <<!
@@ -14,20 +15,20 @@ set logscale y
 set title "KP Proof Steps"
 set xlabel "N. of items"
 set ylabel "Steps"
-set yrange [1:*]
-plot "$data" u 1:2 title "Avg. of 1000", 2**x t "2^x", 1.76**(x+3) t "1.76^(x+3)"
+set yrange [1:1e+16]
+plot "$data1" u 1:2 title "Avg. of 1000", 2**x t "2^x", 1.76**(x+3) t "1.76^(x+3)"
 # PLOTING CURVE OF FINDING
-unset logscale y
 set term $term
 set datafile separator ";"
 set output "find.$term"
 set style data linespoints
 set grid
+set logscale y
 set title "KP Proof Steps"
 set xlabel "N. of items"
 set ylabel "Steps"
-set yrange [1:*]
-plot "$data" u 1:3 title "Avg. of 1000"
+set yrange [1:1e+16]
+plot "$data2" u 1:2 title "Avg. of 1000", 2**x t "2^x", 1.55**(x-4) t "1.55^(x-4)"
 !
 
 
