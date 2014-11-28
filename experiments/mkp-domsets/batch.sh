56aed8c15172e9d272097e61f2c71be67851b8e3
Marcos Daniel Valadao Baroni <mbaroni@athena.ninfa.inf.ufes.br>
2014-11-28 17:27:24 -0200

    batch more dims.

--- experiments/mkp-domsets/batch.sh
+++ experiments/mkp-domsets/batch.sh
@@ -2,43 +2,36 @@
 ######################################################################
 # Script para testes em lote do algoritmo de backtrack para o KP.
 ######################################################################
-BINDIR="../../bin"
-SCIP=$BINDIR/scip
-RUNSCIP=$BINDIR/runscip
-SCIP2SUMMARY=$BINDIR/scip2summary
-ZPL2LP=$BINDIR/zpl2lp
-KP2ZPL=$BINDIR/kp2zpl
+BINDIR=../../bin
 RANDMKP=$BINDIR/randmkp
-KPBT=$BINDIR/kpbt
-RUNNERHS=../../src/mmkp/haskell/MKP.hs
+DOMSETS=$BINDIR/mkp-domsets
 
 # Checkinkg number of arguments
-if [ $# -lt 4 ]; then
-    echo -e "usage:\n $0 <output> <n_0> <n_f> <# of samples>\n"
-	echo "  output: output CSV file"
+if [ $# -lt 6 ]; then
+    echo -e "usage:\n $0 <output-dir> <n0> <nf> <m0> <mf> <# of samples>\n"
+	echo "  output-dir: directory to place CSV output (name of file will be random)"
 	echo "  [n_0, n_f]: range of number of items"
+	echo "  [m_0, m_f]: range of number of dimension"
 	echo "  # of samples: number of samples for each instance"
     exit 1
 fi
 
-output=${1}
+output=`tempfile -d $1 -s .csv`
 n0=$2
 nf=$3
-nsamples=$4
-
-function parsenumber(){
-python <<!
-from sys import stdin
-print(stdin.readlines())
-!
-}
+m0=$4
+mf=$5
+nsamples=$6
 
 for i in `seq 1 $nsamples`
 do
-	for n in `seq $n0 $nf`
+	for m in `seq $m0 $mf`
 	do
-		num=`$RANDMKP $n 2 | runhaskell $RUNNERHS`
-		echo $n";"$num
+		for n in `seq $n0 $nf`
+		do
+			num=`$RANDMKP $n $m | $DOMSETS`
+			echo "$n;$m;$num" >> $output
+		done
 	done
 done
 
