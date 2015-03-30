fd3d0f24607fc83ee6a397560152c5cfe3f966fb
Marcos Daniel Baroni <marcosdaniel.baroni@gmail.com>
2015-03-30 17:21:43 -0300

    NU bucket.

---
+++ src/mmkp/c/mmkp/nu-bucket.h
@@ -0,0 +1,20 @@
+#ifndef NUBUCKET_H
+#define NUBUCKET_H 1
+
+#include "util.h"
+#include "mkp.h"
+
+typedef struct NUBucket{
+	MKP *mkp;
+	int n;
+	int m;
+}NUBucket;
+
+typedef struct SolBucket{
+	long long minProfit;
+	long long maxProfit;
+	long long 
+}SolBucket;
+
+#endif
+
