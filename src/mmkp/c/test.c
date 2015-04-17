da00526b30f0746db29b6acec10246d22ae6cbf8
Marcos Daniel Baroni <marcosdaniel.baroni@gmail.com>
2015-04-17 17:31:59 -0300

    pequeno batch para testar cardinality-surrogate.

--- src/mmkp/c/test.c
+++ src/mmkp/c/test.c
@@ -16,8 +16,10 @@
 int execute_surrogate_research(int argc, char **argv){
 	MKP *mkp, *mkp2;
 	FILE *input;
-	int i, m, nmax, *multps;
+	int i, n, m, nmax, *multps;
 	clock_t c0, cf;
+
+	double *x, sum;
 
 	/* default arguments */
 	input = stdin;
@@ -36,6 +38,7 @@ int execute_surrogate_research(int argc, char **argv){
 	/* reading instance */
 	mkp = mkp_read_from_file(input);
 	m = mkp->m;
+	n = mkp->n;
 	fclose(input);
 
 	multps = (int*)malloc(m*sizeof(int));
@@ -47,7 +50,7 @@ int execute_surrogate_research(int argc, char **argv){
 		mkp2 = mkp_surrogate(mkp, multps);
 		nmax = mkp_max_cardinality(mkp2);
 
-		printf("(%d, %d): %d\n", i+1, i+2, nmax);
+		printf("d;%d;%d;%d\n", n, m, nmax);
 
 		mkp_free(mkp2);
 	}
@@ -56,8 +59,24 @@ int execute_surrogate_research(int argc, char **argv){
 	multps = int_array_init(multps, m, 1);
 	mkp2 = mkp_surrogate(mkp, multps);
 	nmax = mkp_max_cardinality(mkp2);
-	printf("(all): %d\n", nmax);
+	printf("a;%d;%d;%d\n", n, m, nmax);
 	mkp_free(mkp2);
+
+	/* checking LP */
+	x = mkp_get_lp_sol(mkp);;
+	sum = 0;
+	for( i = 0 ;  i < n ; i++ )
+		sum += x[i];
+	printf("l;%d;%d;%.2lf\n", n, m, sum);
+	free(x);
+
+	/* checking exact */
+	x = mkp_solve_with_scip(mkp, 600, 1.0, 0);
+	nmax = 0;
+	for( i = 0 ; i < n ; i++ )
+		if( x[i] >= 1.0 )
+			nmax++;
+	printf("s;%d;%d;%d\n", n, m, nmax);
 
 	/* freeing */
 	free(multps);
