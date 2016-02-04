29f71f5c552f1ca605d380cb28edc40fb514db0a
mbaroni <marcosdaniel.baroni@gmail.com>
2016-02-04 16:55:12 -0200

    mkp-sfl-core.c adicionado.

--- src/mmkp/c/mkp-sfl-core.c
+++ src/mmkp/c/mkp-sfl-core.c
@@ -12,10 +12,17 @@ int print_usage(int argc, char **argv){
 	FILE *out;
 
 	out = stdout;
-	fprintf(out, " usage: %s <input file> <n of memeplex> <size of memeplex> <size of submemeplex> <n of iterations> <n of subiterations> [seed=time(null)]\n", argv[0]);
+	fprintf(out, " usage: %s <input file> <n of memeplex> <size of memeplex> <size of submemeplex> <n of iterations> <n of subiterations> [cross type=(1|2|3)] [new sol type=(1|2)] [seed=time(null)]\n", argv[0]);
 	fprintf(out, " Shuffled Frog Leaping Algorithm for MKP.\n");
 	fprintf(out, " - input file: a MKP instance. If no file is given, instance is read from stdin.\n");
 	fprintf(out, " - Program outputs \"<profit of solution>;<best iter>\"\n");
+	fprintf(out, " - cross types:\n");
+	fprintf(out, "    1 - 50%% from father\n");
+	fprintf(out, "    2 - 20%% from father\n");
+	fprintf(out, "    3 - 10%% from father\n");
+	fprintf(out, " - new solutions types:\n");
+	fprintf(out, "    1 - random solution\n");
+	fprintf(out, "    2 - random solution + local search (experimental)\n");
 
 	return 1;
 }
@@ -45,13 +52,15 @@ int execute_sfl_mkp(int argc, char **argv){
 	q = atoll(argv[4]);
 	niter = atoll(argv[5]);
 	subniter = atoll(argv[6]);
-	if( argc > 7 ) seed = atoll(argv[7]);
+	if( argc > 7 ) cross = atoll(argv[7]);
+	if( argc > 8 ) newsol = atoll(argv[8]);
+	if( argc > 9 ) seed = atoll(argv[9]);
 	srand(seed);
 
 	/* reading problem instance */
 	mkp = mkp_read_from_file(input);
 	fclose(input);
-	sfli = mkp_sfl_interface();
+	sfli = mkp_sfl_interface(cross, newsol);
 
 	/* solving problem */
 	c0 = clock();
@@ -59,8 +68,9 @@ int execute_sfl_mkp(int argc, char **argv){
 	cf = clock();
 
 	/* output */
+	printf(";");
 	mkpnum_fprintf(stdout, sol->obj);
-	fprintf(stdout, ";%d;%f\n", best_iter, ((cf-c0)/(float)CLOCKS_PER_SEC));
+	mkpnum_fprintf(";%d;%f\n", best_iter, ((cf-c0)/(float)CLOCKS_PER_SEC));
 
 	/* freeing */
 	mkpsol_free(sol);
@@ -333,7 +343,7 @@ int execute_sfl_mkp_core_batch(int argc, char **argv){
 }
 
 int main(int argc, char **argv){
-	return execute_sfl_mkp(argc, argv);
+	return execute_sfl_mkp_core(argc, argv);
 	//return execute_sfl_mkp_core_batch(argc, argv);
 }
 
