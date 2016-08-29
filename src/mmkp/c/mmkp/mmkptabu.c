21b322edacf99c82fa5aca6df276cab816c24638
Marcos Danie Baroni <marcosdaniel.baroni@gmail.com>
2016-08-29 19:03:26 -0300

    excluindo alguns arquivos

--- src/mmkp/c/mmkp/mmkptabu.c
+++
@@ -1,6 +0,0 @@
-#include "mmkp/mkp/mkp.h"
-#include "mmkptabu.h"
-
-MMKPSol *mmkp_tabu(MMKP *mmkp, int max_iter){
-}
-
--- src/mmkp/c/mmkp/mmkptabu.h
+++
@@ -1,8 +0,0 @@
-#ifndef MMKP_H
-#define MMKP_H 1
-
-
-MMKPSol *mmkp_tabu(MMKP *mmkp, int max_iter);
-
-#endif
-
